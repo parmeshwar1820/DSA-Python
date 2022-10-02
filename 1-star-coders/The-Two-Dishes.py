e14986724e49ac016b4d64e3200cf7060cd015a3
parmeshwar1820 <114507943+parmeshwar1820@users.noreply.github.com>
2022-10-02 22:34:59 +0530

    incomplete

---
+++ 1-star-coders/The-Two-Dishes.py
@@ -0,0 +1,12 @@
+'''
+Problem
+---
+Input Format
+The first line of input contains a single integer TT, denoting the number of testcases. The description of the TT testcases follows.
+The first and only line of each test case contains two space-separated integers NN, SS, denoting the maximum tastiness and the sum of tastiness of the two dishes, 
+respectively.
+
+Output Format
+For each testcase, output a single line containing the maximum absolute difference between the tastiness of the two dishes.
+'''
+
